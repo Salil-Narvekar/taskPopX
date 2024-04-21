2afffe1cd01f24a0c95dd85df0608b5cbf8869e8
Salil Narvekar <salil.enpointe@gmail.com>
2024-04-21 15:39:40 +0530

    commit

--- src/Components/LandingScreen.jsx
+++ src/Components/LandingScreen.jsx
@@ -14,16 +14,28 @@ const LandingScreen = () => {
     }, [])
 
     return (
-        <div className='flex flex-col min-h-screen overflow-hidden'>
-            <div className='flex-grow'></div>
-            <div className='grid grid-rows-2 gap-2 justify-items-start pl-4 pr-4 pb-8'>
-                <div>
-                    <HeaderText text='Welcome to PopX' />
-                    <SubHeaderText text='Lorem ipsum dolor sit amet, consectetur adipiscing elit,' />
+        <div className="flex flex-col min-h-screen bg-gray-100">
+            <div className="flex-grow"></div>
+            <div className="grid grid-cols-1 gap-4 justify-items-center px-4 py-8 md:justify-items-start md:px-8">
+                <div className="text-center">
+                    <HeaderText text="Welcome to PopX" />
+                    <SubHeaderText text="Lorem ipsum dolor sit amet, consectetur adipiscing elit," />
                 </div>
-                <div className='grid grid-rows-2 gap-2 w-full'>
-                    <Button name='createAccount' buttonLable='Create Account' color='primary' onClick={() => navigate('/signupScreen')} />
-                    <Button name='login' buttonLable='Already Registered ? Login' color='grey' onClick={() => navigate('/loginScreen')} />
+                <div className="grid grid-rows-1 md:grid-cols-2 gap-2 w-full">
+                    <Button
+                        name="createAccount"
+                        buttonLable="Create Account"
+                        color="primary"
+                        onClick={() => navigate('/signupScreen')}
+                        className="w-full"
+                    />
+                    <Button
+                        name="login"
+                        buttonLable="Already Registered? Login"
+                        color="gray"
+                        onClick={() => navigate('/loginScreen')}
+                        className="w-full"
+                    />
                 </div>
             </div>
         </div>
