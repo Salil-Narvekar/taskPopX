ba8da541916d3a7b89fe6ee148797e6120757b18
Salil Narvekar <salil.enpointe@gmail.com>
2024-04-21 13:13:36 +0530

    commit

--- src/Components/LandingScreen.jsx
+++ src/Components/LandingScreen.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useContext} from 'react'
+import React, { useEffect, useContext } from 'react'
 import { useNavigate } from "react-router-dom";
 import { LoggedUserDetails } from '../App';
 import HeaderText from './HeaderText'
@@ -10,19 +10,17 @@ const LandingScreen = () => {
     const loggedUserDetails = useContext(LoggedUserDetails);
 
     useEffect(() => {
-        loggedUserDetails.dispatch({type: "setInitialState"});
-    },[])
+        loggedUserDetails.dispatch({ type: "setInitialState" });
+    }, [])
 
     return (
-        <div className='flex flex-col min-h-screen'>
+        <div className='flex flex-col min-h-screen overflow-x-hidden'>
             <div className='flex-grow'></div>
             <div className='grid grid-rows-2 gap-2 justify-items-start pl-4 pr-4 pb-8'>
-
                 <div>
                     <HeaderText text='Welcome to PopX' />
-                    <SubHeaderText text='Lorem ipsum dolor sit amet, consecteture adipiscing elit,' />
+                    <SubHeaderText text='Lorem ipsum dolor sit amet, consectetur adipiscing elit,' />
                 </div>
-
                 <div className='grid grid-rows-2 gap-2 w-full'>
                     <Button name='createAccount' buttonLable='Create Account' color='primary' onClick={() => navigate('/signupScreen')} />
                     <Button name='login' buttonLable='Already Registered ? Login' color='grey' onClick={() => navigate('/loginScreen')} />
